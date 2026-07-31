b181e0d0a020bfb830809bf6b28bcff742c44e08
harish-dhanapal <avatarharish@gmail.com>
2026-07-31 10:49:06 +0530

    update files

---
+++ script.js
@@ -0,0 +1,209 @@
+// Mapping of parent nodes to their entire list of downstream child IDs & connectors
+const lineageMap = {
+    // Cain's Branch
+    'cain-enoch-node': [
+        'cain-enoch-connector', 'irad-node', 'irad-connector', 
+        'mehujael-node', 'mehujael-connector', 'methushael-node', 
+        'methushael-connector', 'lamech-node', 'lamech-connector', 
+        'lamech-children', 'lamech-children-connector'
+    ],
+    'irad-node': [
+        'irad-connector', 'mehujael-node', 'mehujael-connector', 
+        'methushael-node', 'methushael-connector', 'lamech-node', 
+        'lamech-connector', 'lamech-children', 'lamech-children-connector'
+    ],
+    'mehujael-node': [
+        'mehujael-connector', 'methushael-node', 'methushael-connector', 
+        'lamech-node', 'lamech-connector', 'lamech-children', 
+        'lamech-children-connector'
+    ],
+    'methushael-node': [
+        'methushael-connector', 'lamech-node', 'lamech-connector', 
+        'lamech-children', 'lamech-children-connector'
+    ],
+    'lamech-node': [
+        'lamech-connector', 'lamech-children', 'lamech-children-connector'
+    ],
+
+    // Seth's Lineage down to Noah
+    'enosh-node': [
+        'enosh-connector', 'kenan-node', 'kenan-connector',
+        'mahalalel-node', 'mahalalel-connector', 'jared-node',
+        'jared-connector', 'seth-enoch-node', 'seth-enoch-connector',
+        'methuselah-node', 'methuselah-connector', 'seth-lamech-node',
+        'seth-lamech-connector', 'noah-node', 'noah-connector',
+        'noah-children', 'noah-children-connector'
+    ],
+    'kenan-node': [
+        'kenan-connector', 'mahalalel-node', 'mahalalel-connector',
+        'jared-node', 'jared-connector', 'seth-enoch-node', 'seth-enoch-connector',
+        'methuselah-node', 'methuselah-connector', 'seth-lamech-node',
+        'seth-lamech-connector', 'noah-node', 'noah-connector',
+        'noah-children', 'noah-children-connector'
+    ],
+    'mahalalel-node': [
+        'mahalalel-connector', 'jared-node', 'jared-connector',
+        'seth-enoch-node', 'seth-enoch-connector', 'methuselah-node',
+        'methuselah-connector', 'seth-lamech-node', 'seth-lamech-connector',
+        'noah-node', 'noah-connector', 'noah-children', 'noah-children-connector'
+    ],
+    'jared-node': [
+        'jared-connector', 'seth-enoch-node', 'seth-enoch-connector',
+        'methuselah-node', 'methuselah-connector', 'seth-lamech-node',
+        'seth-lamech-connector', 'noah-node', 'noah-connector',
+        'noah-children', 'noah-children-connector'
+    ],
+    'seth-enoch-node': [
+        'seth-enoch-connector', 'methuselah-node', 'methuselah-connector',
+        'seth-lamech-node', 'seth-lamech-connector', 'noah-node',
+        'noah-connector', 'noah-children', 'noah-children-connector'
+    ],
+    'methuselah-node': [
+        'methuselah-connector', 'seth-lamech-node', 'seth-lamech-connector',
+        'noah-node', 'noah-connector', 'noah-children', 'noah-children-connector'
+    ],
+    'seth-lamech-node': [
+        'seth-lamech-connector', 'noah-node', 'noah-connector',
+        'noah-children', 'noah-children-connector'
+    ],
+    'noah-node': [
+        'noah-connector', 'noah-children', 'noah-children-connector'
+    ],
+
+    // Root Generation 2
+    'gen2-children': [
+        'gen2-connector', 'cain-enoch-node', 'cain-enoch-connector',
+        'irad-node', 'irad-connector', 'mehujael-node', 'mehujael-connector',
+        'methushael-node', 'methushael-connector', 'lamech-node', 
+        'lamech-connector', 'lamech-children', 'lamech-children-connector',
+        'enosh-node', 'enosh-connector', 'kenan-node', 'kenan-connector',
+        'mahalalel-node', 'mahalalel-connector', 'jared-node', 'jared-connector',
+        'seth-enoch-node', 'seth-enoch-connector', 'methuselah-node',
+        'methuselah-connector', 'seth-lamech-node', 'seth-lamech-connector',
+        'noah-node', 'noah-connector', 'noah-children', 'noah-children-connector'
+    ]
+};
+
+// Direct mapping of target nodes to their immediate connector lines
+const connectorMap = {
+    'gen2-children': 'gen2-connector',
+    'cain-enoch-node': 'cain-enoch-connector',
+    'irad-node': 'irad-connector',
+    'mehujael-node': 'mehujael-connector',
+    'methushael-node': 'methushael-connector',
+    'lamech-node': 'lamech-connector',
+    'lamech-children': 'lamech-children-connector',
+
+    // Seth's Connectors
+    'enosh-node': 'enosh-connector',
+    'kenan-node': 'kenan-connector',
+    'mahalalel-node': 'mahalalel-connector',
+    'jared-node': 'jared-connector',
+    'seth-enoch-node': 'seth-enoch-connector',
+    'methuselah-node': 'methuselah-connector',
+    'seth-lamech-node': 'seth-lamech-connector',
+    'noah-node': 'noah-connector',
+    'noah-children': 'noah-children-connector'
+};
+
+// Parent map for auto-scroll on collapse
+const parentMap = {
+    'gen2-children': 'root',
+    'cain-enoch-node': 'gen2-children',
+    'enosh-node': 'gen2-children',
+    'irad-node': 'cain-enoch-node',
+    'mehujael-node': 'irad-node',
+    'methushael-node': 'mehujael-node',
+    'lamech-node': 'methushael-node',
+    'lamech-children': 'lamech-node',
+
+    // Seth line parents
+    'kenan-node': 'enosh-node',
+    'mahalalel-node': 'kenan-node',
+    'jared-node': 'mahalalel-node',
+    'seth-enoch-node': 'jared-node',
+    'methuselah-node': 'seth-enoch-node',
+    'seth-lamech-node': 'methuselah-node',
+    'noah-node': 'seth-lamech-node',
+    'noah-children': 'noah-node'
+};
+
+function revealNext(targetId) {
+    const targetElement = document.getElementById(targetId);
+    if (!targetElement) return;
+
+    const isHidden = targetElement.classList.contains('hidden');
+
+    if (isHidden) {
+        // --- EXPAND BRANCH ---
+
+        // Auto-collapse Cain's tree if opening Seth's branch
+        if (targetId === 'enosh-node') {
+            collapseDescendants('cain-enoch-node');
+            hideElement('cain-enoch-node');
+            hideElement('cain-enoch-connector');
+        }
+
+        // Auto-collapse Seth's tree if opening Cain's branch
+        if (targetId === 'cain-enoch-node') {
+            collapseDescendants('enosh-node');
+            hideElement('enosh-node');
+            hideElement('enosh-connector');
+        }
+
+        // Show target element and its immediate connector
+        targetElement.classList.remove('hidden');
+        if (connectorMap[targetId]) {
+            showElement(connectorMap[targetId]);
+        }
+
+        // Smooth center scroll to newly expanded node
+        scrollToNode(targetId);
+
+    } else {
+        // --- COLLAPSE BRANCH & ALL DESCENDANTS ---
+        hideElement(targetId);
+        if (connectorMap[targetId]) {
+            hideElement(connectorMap[targetId]);
+        }
+        collapseDescendants(targetId);
+
+        // Scroll back up to the parent node
+        if (parentMap[targetId]) {
+            scrollToNode(parentMap[targetId]);
+        }
+    }
+}
+
+// Smooth scroll centering helper
+function scrollToNode(nodeId) {
+    requestAnimationFrame(() => {
+        setTimeout(() => {
+            const element = document.getElementById(nodeId);
+            if (!element) return;
+
+            element.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+                inline: 'center'
+            });
+        }, 120);
+    });
+}
+
+// Helpers
+function hideElement(id) {
+    const el = document.getElementById(id);
+    if (el) el.classList.add('hidden');
+}
+
+function showElement(id) {
+    const el = document.getElementById(id);
+    if (el) el.classList.remove('hidden');
+}
+
+function collapseDescendants(parentId) {
+    if (lineageMap[parentId]) {
+        lineageMap[parentId].forEach(childId => hideElement(childId));
+    }
+}
